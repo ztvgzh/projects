d928f02191cd08a274267a19ee74031bdb2fcb1d
Gulshad <azgulj@gmail.com>
2024-02-17 16:35:28 +0300

    8th project

---
+++ time_math_challenge/main.py
@@ -0,0 +1,34 @@
+import random
+import time
+
+OPERATORS = ["+", "-", "*"]
+MIN_OPERAND = 3
+MAX_OPERAND = 12
+TOTAL_PROBLEMS = 10
+
+def generate_problem():
+    left = random.randint(MIN_OPERAND, MAX_OPERAND)
+    right = random.randint(MIN_OPERAND, MAX_OPERAND)
+    operator = random.choice(OPERATORS)
+
+    expr = str(left) + operator + str(right)
+    answer = eval(expr)
+    return expr, answer
+
+wrong = 0
+input("press enter to start!")
+print("-------------------------")
+start_time = time.time()
+
+for i in range(TOTAL_PROBLEMS):
+    expr, answer = generate_problem()
+    while True:
+        guess = input(f"Problem # {str(i+1)}: {expr} = ")
+        if guess == str(answer):
+            break
+        wrong += 1
+
+end_time = time.time()
+total_time = end_time-start_time
+print("-------------------------")
+print("Nice work! You finished in", round(total_time, 2), "seconds!")
